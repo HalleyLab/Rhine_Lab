57de94d2e7d8b62dcf160b59cf697e6039a6f87b
HalleYi <ZJUHalleY@outlook.com>
2026-08-10 16:43:35 -0400

    Configure Supabase connection

--- js/rhine-lab-config.js
+++ js/rhine-lab-config.js
@@ -4,8 +4,8 @@
     // The publishable/anon key is safe to use in a browser when RLS is enabled.
     // Never place a service_role key in this file.
     window.RHINE_LAB_CONFIG = Object.assign({
-        supabaseUrl: '',
-        supabasePublishableKey: '',
+        supabaseUrl: 'https://tyjaprmkrjxgccsnqwog.supabase.co',
+        supabasePublishableKey: 'sb_publishable_7OQarZUrwQw9UdqTfGFaQw_vO5AqAQa',
         labId: '',
         autoInitializeLab: false,
         seedUrl: './data/seed.json'
