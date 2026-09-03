b95bc1a58367565503ade856756bf2fa8f14bd86
HalleYi <ZJUHalleY@outlook.com>
2026-09-02 20:23:24 -0400

    Open browser extension in editable workspace

--- browser-extension/popup.js
+++ browser-extension/popup.js
@@ -1,7 +1,7 @@
 'use strict';
 
 document.getElementById('openRhineLab').addEventListener('click', function () {
-  chrome.tabs.create({ url: 'https://halleylab.github.io/Rhine_Lab/' }, function () {
+  chrome.tabs.create({ url: 'https://halleylab.github.io/Rhine_Lab/?app=1#dashboard' }, function () {
     window.close();
   });
 });
